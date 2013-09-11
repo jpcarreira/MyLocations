3efca88d74fde3d615308fa06b195fa0c16b80fe
jpcarreira <jpmavcarreira@gmail.com>
2013-09-11 17:33:21 +0100

    defined MKCoordinateRegion that includes all saved locations ref #10

--- MyLocations/MyLocations/MapViewController.m
+++ MyLocations/MyLocations/MapViewController.m
@@ -31,6 +31,12 @@ NSArray *locations;
 {
     [super viewDidLoad];
 	[self updateLocations];
+    
+    // enabling showing user's location by default
+    if([locations count] > 0)
+    {
+        [self showLocations];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -73,6 +79,63 @@ NSArray *locations;
     [self.mapView addAnnotations:locations];
 }
 
+/**
+ * defines a region around user's saved locations
+ * (note that this method doesn't work in Location objects, it assumes that the objects in the annotations array conform in the MKAnnotation protocol
+ */
+-(MKCoordinateRegion)regionForAnnotations:(NSArray *)annotations
+{
+    MKCoordinateRegion region;
+    
+    // if there are no annotation the map is centered in user's current location
+    if([annotations count] == 0)
+    {
+        region = MKCoordinateRegionMakeWithDistance(self.mapView.userLocation.coordinate, 1000, 1000);
+    }
+    
+    // if there's only one annotation the map is centered in that annotation
+    else if([annotations count] == 1)
+    {
+        id<MKAnnotation> annotation = [annotations lastObject];
+        region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 1000, 1000);
+    }
+    
+    // if there's 2 or more locations we calculate the extent they reach and add some padding
+    else
+    {
+        // top left coordinate
+        CLLocationCoordinate2D topLeftCoordinate;
+        topLeftCoordinate.latitude = -90;
+        topLeftCoordinate.longitude = 180;
+        
+        // bottom right coordinate
+        CLLocationCoordinate2D bottomRightCoordinate;
+        bottomRightCoordinate.latitude = 90;
+        bottomRightCoordinate.longitude = -180;
+        
+        // fast enumeration for update the default coordinates above
+        for(id<MKAnnotation> annotation in annotations)
+        {
+            topLeftCoordinate.latitude = fmax(topLeftCoordinate.latitude, annotation.coordinate.latitude);
+            topLeftCoordinate.longitude = fmin(topLeftCoordinate.longitude, annotation.coordinate.longitude);
+            
+            bottomRightCoordinate.latitude = fmin(bottomRightCoordinate.latitude, annotation.coordinate.latitude);
+            bottomRightCoordinate.longitude = fmax(bottomRightCoordinate.longitude, annotation.coordinate.longitude);
+        }
+        
+        // defining the padding
+        const double extraSpace = 1.1;
+        
+        region.center.latitude = topLeftCoordinate.latitude - (topLeftCoordinate.latitude - bottomRightCoordinate.latitude) / 2.0;
+        region.center.longitude = topLeftCoordinate.longitude - (topLeftCoordinate.longitude - bottomRightCoordinate.longitude) / 2.0;
+        
+        region.span.latitudeDelta = fabs(topLeftCoordinate.latitude - bottomRightCoordinate.latitude) * extraSpace;
+        region.span.longitudeDelta = fabs(topLeftCoordinate.longitude - bottomRightCoordinate.longitude) * extraSpace;
+    }
+    
+    return [self.mapView regionThatFits:region];
+}
+
 
 #pragma mark - IBActions
 
@@ -86,7 +149,9 @@ NSArray *locations;
 
 -(void)showLocations
 {
-    
+    // zooming the map around saved locations
+    MKCoordinateRegion region = [self regionForAnnotations:locations];
+    [self.mapView setRegion:region animated:YES];
 }
 
 
